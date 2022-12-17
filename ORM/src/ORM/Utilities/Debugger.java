b265e5bdf9f6289ace13515ec4e22a769a55ae8b
0xOrigin <egyahmed.ezzat120@gmail.com>
2022-12-18 00:25:58 +0200

    Turn off Debugger

--- ORM/src/ORM/Utilities/Debugger.java
+++ ORM/src/ORM/Utilities/Debugger.java
@@ -15,7 +15,7 @@ public class Debugger {
      */
     public static void printQuery(String query, boolean On){
         
-//        On = false; // For self-disabling
+        On = false; // For self-disabling
 
         if(On)
             System.out.println(query);
